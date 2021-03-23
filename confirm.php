ae813543ae6dffb7caebddb4351a60a5aeb70fd2
DESKTOP-Q99RSHA\gengu <gen.guitar1090@gmail.com>
2021-03-23 16:40:24 +0900

    確認ページ追加

---
+++ confirm.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- リセットCSS読み込み -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
+	<!-- CSS読み込み -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- レスポンシブ読み込み -->
+	<link rel="stylesheet" href="css/responsive.css">
+
+	<title>株式会社Flying Corps</title>
+
+	<!-- jQuery CDN読み込み -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+
+	<div class="wrapper">
+
+		<header>
+			<div class="container">
+				<div class="header-inner">
+					<div class="header-left logo">
+						<a href="">
+							<img src="img/logo.png" alt="">
+						</a>
+					</div>
+					<div class="gnav">
+						<nav>
+							<ul>
+								<li>
+									<a href="#about-us">
+										私たちについて
+									</a>
+								</li>
+								<li>
+									<a href="#service">
+										事業内容
+									</a>
+								</li>
+								<li>
+									<a href="#company">
+										会社概要
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<div class="contact-icon">
+						<a href="#contact">
+							<img src="img/contact-icon.png" alt="">
+						</a>
+					</div>
+				</div>
+			</div>
+		</header>
+		
+        
+
+		<footer>
+			<img src="img/footer-img.png" alt="" class="footer-img">
+			<div class="container">
+				<div class="section-inner">
+					<img src="img/logo.png" alt="" class="footer-logo">
+					<div class="footer-nav">
+						<nav>
+							<ul>
+								<li><a href="">ホーム</a></li>
+								<li><a href="">私たちについて</a></li>
+								<li><a href="">事業内容</a></li>
+								<li><a href="">会社概要</a></li>
+								<li><a href="">お問い合わせ</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+	</div>
+
+	<!-- JS読み込み -->
+	<script src="js/main.js"></script>
+</body>
+</html>
